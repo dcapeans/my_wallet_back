2b57e08b724c182d1b60bf9b247f6450f880abaa
Daniel Capeans <dcapeans@gmail.com>
2021-06-21 20:42:51 -0300

    created initial structure

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+
+
+app.listen(4000, () => {
+    console.log("Server listening at port 4000")
+})

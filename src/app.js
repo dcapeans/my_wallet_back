53c049b589e9042b65d41167a9096d04390b575f
Daniel Capeans <dcapeans@gmail.com>
2021-06-23 19:35:31 -0300

    added transactions routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import connection from './database.js'
 import bcrypt from 'bcrypt'
 import { v4 as uuidv4 } from 'uuid';
+import dayjs from 'dayjs'
 
 const app = express()
 app.use(express.json())
@@ -21,14 +22,26 @@ app.post('/sign-in', async (req, res) => {
     const user = result.rows[0]
 
     if(user && bcrypt.compareSync(password, user.password)){
-        const token = uuidv4()
+        const session = await connection.query(`
+            SELECT *
+            FROM sessions
+            WHERE sessions.user_id = $1
+        `, [user.id])
 
+        const activeSession = session.rows[0]
+        if(activeSession){
+            return res.send(activeSession.token)
+        }
+
+        const token = uuidv4()
         await connection.query(`
             INSERT INTO sessions 
             (user_id, token)
             VALUES ($1, $2)
         `, [user.id, token])
-        res.sendStatus(200).send(token)
+        
+        console.log(token)
+        res.send(token)
     } else {
         res.sendStatus(401)
     }  
@@ -68,10 +81,93 @@ app.post('/sign-up', async (req, res) =>{
     }
 })
 
+// all transactions route
+app.get('/transactions', async (req, res) => {
+    try {
+        const authorization = req.header('Authorization')
+        const token = authorization?.replace('Bearer ', '')
+
+        if(!token) return res.sendStatus(401)
+
+        const result = await connection.query(`
+            SELECT transactions.type, transactions.value, transactions.transaction_date
+            FROM sessions
+            JOIN users
+            ON sessions.user_id = users.id
+            JOIN transactions
+            ON transactions.user_id = users.id
+            WHERE sessions.token = $1
+        `, [token])
+
+        const transactions = result.rows
+
+        if(transactions){
+            res.send(transactions)
+            return
+        } else{
+            res.sendStatus(401)
+        }
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+})
 // newIncome route
+app.post('/newIncome', async (req, res) => {
+    try {
+        const { type, value } = req.body
+        const authorization = req.header('Authorization')
+        const token = authorization?.replace('Bearer ', '')
 
+        if(type !== 'income') return res.sendStatus(400)
+        if(!token) return res.sendStatus(401)
+
+        const user = await connection.query(`
+            SELECT *
+            FROM sessions
+            WHERE sessions.token = $1
+        `, [token])
+
+        const userId = user.rows[0].user_id
+        const transaction_date = dayjs().format()
+        await connection.query(`
+            INSERT INTO transactions (type, value, user_id, transaction_date)
+            VALUES ($1, $2, $3, $4)
+        `, [type, value, userId, transaction_date])
+
+        res.sendStatus(201)
+    } catch (error) {
+        res.sendStatus(500)
+    }
+})
 // newOutflow route
+app.post('/newOutflow', async (req, res) => {
+    try {
+        const { type, value } = req.body
+        const authorization = req.header('Authorization')
+        const token = authorization?.replace('Bearer ', '')
 
+        if(type !== 'outflow') return res.sendStatus(400)
+        if(!token) return res.sendStatus(401)
+
+        const user = await connection.query(`
+            SELECT *
+            FROM sessions
+            WHERE sessions.token = $1
+        `, [token])
+        
+        const userId = user.rows[0].user_id
+        const transaction_date = dayjs().format()
+        await connection.query(`
+            INSERT INTO transactions (type, value, user_id, transaction_date)
+            VALUES ($1, $2, $3, $4)
+        `, [type, value, userId, transaction_date])
+
+        res.sendStatus(201)
+    } catch (error) {
+        res.sendStatus(500)
+    }
+})
 
 app.listen(4000, () => {
     console.log("Server listening at port 4000")

1cc12ba1f00a0c958edd528dbb48a2d984d03a71
Daniel Capeans <dcapeans@gmail.com>
2021-06-23 00:59:59 -0300

    added signIn and signUp routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,69 @@
 import express from 'express'
 import cors from 'cors'
+import connection from './database.js'
+import bcrypt from 'bcrypt'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
+// sign-in route
+app.post('/sign-in', async (req, res) => {
+    try {
+    const { email, password } = req.body
 
+    const result = await connection.query(`
+        SELECT * 
+        FROM users
+        WHERE email = $1    
+    `, [email])
+
+    const user = result.rows[0]
+
+    if(user && bcrypt.compareSync(password, user.password)){
+        res.sendStatus(200)
+    } else {
+        res.sendStatus(401)
+    }  
+    } catch (error) {
+        res.sendStatus(500)
+    }
+})
+
+// sign-up route
+app.post('/sign-up', async (req, res) =>{
+    try {
+    const { name, email, password } = req.body
+    const passwordHash = bcrypt.hashSync(password, 10)
+    
+    const user = await connection(`
+        SELECT * 
+        FROM users
+        WHERE email = $1
+    `, [email])
+
+    const userExists = user.rows[0]
+
+    if(userExists){
+        return res.sendStatus(409)
+    }
+
+    await connection.query(`
+        INSERT INTO users 
+        (name, email, password)
+        VALUES ($1, $2, $3)
+    `, [name, email, passwordHash])
+
+    res.sendStatus(201)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+})
+
+// newIncome route
+
+// newOutflow route
 
 
 app.listen(4000, () => {
